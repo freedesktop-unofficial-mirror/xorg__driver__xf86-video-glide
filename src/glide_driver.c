532d3636f31d45fef7b1b0ba8bc3cc61e429c90e
Egbert Eich <eich@suse.de>
2004-03-14 08:33:20 +0000

    Importing vendor version xf86-4_4_99_1 on Sun Mar 14 00:26:39 PST 2004

--- src/glide_driver.c
+++ src/glide_driver.c
@@ -45,7 +45,7 @@
    * Support static loading.  
 */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c,v 1.27 2001/08/07 07:04:46 keithp Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/glide/glide_driver.c,v 1.28 2002/01/04 21:22:30 tsi Exp $ */
 
 #include "xaa.h"
 #include "xf86Cursor.h"
