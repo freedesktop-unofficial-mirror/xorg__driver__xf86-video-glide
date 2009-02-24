freedesktop-unofficial-mirror/xorg__driver__xf86-video-glide

commit 62922e27c80ac392fc35a347b2c8163fcec7decf

Use "no" bus, not ISA.

src/glide_driver.c
@@ -392,12 +392,12 @@ GLIDEProbe(DriverPtr drv, int flags)
       {
         int entity;
         /* Match */
-        entity = xf86ClaimIsaSlot(drv, 0, dev, TRUE);
+        entity = xf86ClaimNoSlot(drv, 0, dev, TRUE);
         pScrn = NULL;
 
         /* Allocate a ScrnInfoRec and claim the slot */
-        if ((pScrn = xf86ConfigIsaEntity(pScrn, 0, entity, NULL, NULL,
-					       NULL, NULL, NULL, NULL))) {
+        if ((pScrn = xf86AllocateScreen(drv, 0))) {
+	    xf86AddEntityToScreen(pScrn, entityIndex);
 	    
 	    /* I'm not going to "claim" the glide device since no other driver than this can drive it */
 	    /* (A glide device is not a PCI device) */

commit 0cdda109b9fc3deb5ad860f144d3061551ad6058

Rename entity to entityIndex to fix build failure
Regression introduced in commit 62922e27c80ac392fc35a347b2c8163fcec7decf.

src/glide_driver.c
@@ -390,9 +390,9 @@ GLIDEProbe(DriverPtr drv, int flags)
       GlideDevice = xf86SetIntOption(dev->options, "GlideDevice", 0);
       if (GlideDevice == sst)
       {
-        int entity;
+        int entityIndex;
         /* Match */
-        entity = xf86ClaimNoSlot(drv, 0, dev, TRUE);
+        entityIndex = xf86ClaimNoSlot(drv, 0, dev, TRUE);
         pScrn = NULL;
 
         /* Allocate a ScrnInfoRec and claim the slot */