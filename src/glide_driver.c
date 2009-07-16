48fddeb136aba247e06688d8127ee133ae3b8dd2
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-16 11:38:29 +1000

    Update to xextproto 7.1 support.
    DPMS header was split into dpms.h (client) and dpmsconst.h (server). Drivers
    need to include dpmsconst.h if xextproto 7.1 is available.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/glide_driver.c
+++ src/glide_driver.c
@@ -60,8 +60,13 @@
 #include "micmap.h"
 #include "xf86DDC.h"
 #include "globals.h"
+#ifdef HAVE_XEXTPROTO_71
+#include <X11/extensions/dpmsconst.h>
+#else
 #define DPMS_SERVER
 #include <X11/extensions/dpms.h>
+#endif
+
 #include "fb.h"
 #include "xf86cmap.h"
 #include "shadowfb.h"
